36d5497c355741d48532fc4f40d5e1da59296e81
Lomeli89 <19380605@loscabos.tecnm.mx>
2022-11-14 03:43:43 -0700

    New Upload

---
+++ ventanas/RecWid.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QWidget
+
+from ChatBot import Ui_Form as form
+
+class Widget(QWidget,form):
+    def __init__(self, parent, none):
+        super(Widget, self).__init__(parent)
+        QWidget.__init__(self)
+        self.SetupUi(self)
