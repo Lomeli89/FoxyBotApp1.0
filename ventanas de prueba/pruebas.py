bd5e92f83ce95575e52ca384d4e73c402c869f2d
Lomeli89 <19380605@loscabos.tecnm.mx>
2022-11-23 00:14:12 -0700

    organizando.....2

--- ventanas de prueba/pruebas.py
+++ ventanas de prueba/pruebas.py
@@ -12,14 +12,31 @@ temp_line_pass = str
 blista_email = str
 blista_pass = str
 
-temp_index_email = str
+temp_index_email = ""
 temp_index_pass = str
 
 
+##### verificacion de email
+
+problema_email = ""
+
+
+signos = ['.','_','-']
+numeros = ['0','1','2','3','4','5','6','7','8','9']
+dominios = ['gmail','hotmail','msn','yahoo','outlook','live','loscabos']
+minusculas = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+mayusculas = []
+extensiones = ['com','net','com.mx','ea','es','mx','org','gob','tecnm.mx']
+
+
+
+
+################################################
 while True:
     print("Menu de sesion")
     print("1. Iniciar sesion")
     print("2. Registrarme")
+    print("3. Salir")
 
     menu = int(input("-> "))
     if menu == 1:
@@ -45,31 +62,54 @@ while True:
             print("error: intente de nuevo")
 
     elif menu == 2:
+        for x in mayusculas:
+            mayusculas.append(x.upper())
         print("Registro")
         temp_line_email = str(input("Email -> "))
-        blista_email = (temp_line_email in listaEmail)
-        if blista_email == True:
-            print("Usuario ya registrado, intente con uno nuevo")
 
+        if temp_index_email.find('@'):
+            nuevo_email = temp_index_email.split('@')
+            usuario = nuevo_email[0]
+            resto = nuevo_email[1]
+            continuacion = resto.split('.')
+            dominio = continuacion[0]
+            terminacion = continuacion[1]
+            for x in usuario:
+                if x in signos or x in numeros or x in minusculas or x in mayusculas:
+                    if dominio in dominios:
+                        if terminacion in extensiones:
+                            problema_email = "El email es correcto"
+                            blista_email = (temp_line_email in listaEmail)
+                            if blista_email == True:
+                                print("Usuario ya registrado, intente con uno nuevo")
+                            else:
+                                temp_line_pass = str(input("contraseña -> "))
+
+                                listaEmail.append(temp_line_email)
+                                listaPass.append(temp_line_pass)
+
+                                temp_index_email = listaEmail.index(temp_line_email)
+                                temp_line_pass = listaPass.index(temp_line_pass)
+
+                                index_list_email.append(temp_line_email)
+                                index_list_pass.append(temp_line_pass)
+                        else:
+                            problema_email += "La terminación no es común pero puede ser valido \n"
+                    else:
+                        problema_email += "El dominio  no es reconocido pero puede ser privado \n"
+                else:
+                    problema_email += "El valor " + x + " no es valido para un correo \n"
         else:
-
-
-            temp_line_pass = str(input("contraseña -> "))
-
-            listaEmail.append(temp_line_email)
-            listaPass.append(temp_line_pass)
-
-            temp_index_email = listaEmail.index(temp_line_email)
-            temp_line_pass = listaPass.index(temp_line_pass)
-
-            index_list_email.append(temp_line_email)
-            index_list_pass.append(temp_line_pass)
+            problema_email +="El correo no tiene un @ \n"
+        print(problema_email)
 
 
 
 
 
 
+    elif menu == 3:
+        print("adios")
 
 
 
